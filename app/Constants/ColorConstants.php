831a253534c3e57a4cf35ea4f20f9728a2be39f1
raflimuhammadd <raflimuhh09@gmail.com>
2025-12-13 17:57:31 +0700

    Add: constant, shared-data

---
+++ app/Constants/ColorConstants.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Constants;
+
+// static class tidak perlu di instansiasi
+// konstanta array
+class ColorConstants {
+    const COLORS = [
+        "#10b981", // green
+        "#06b6d4", // cyan
+        "#3b82f6", // blue
+        "#6366f1", // indigo
+        "#d946ef", // purple
+        "#06b6d4", // cyan
+        "#f43f5e", // merah
+        "#84cc16", // kuning
+    ];
+}

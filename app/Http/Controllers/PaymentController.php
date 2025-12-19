fee0ce1ae108439cbcf42a4a8152564cedee7515
raflimuhammadd <raflimuhh09@gmail.com>
2025-12-19 08:40:57 +0700

    Add: feature add data to payment method

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\MessageType;
+use App\Enums\PaymentType;
+use App\Http\Requests\PaymentRequest;
 use App\Http\Resources\PaymentResource;
 use App\Models\Payment;
 use Illuminate\Http\Request;
@@ -9,6 +12,8 @@ use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Throwable;
 
 class PaymentController extends Controller implements HasMiddleware
 {
@@ -61,5 +66,45 @@ class PaymentController extends Controller implements HasMiddleware
             ],
 
         ]);
+    }
+
+        // method create
+    public function create(): Response
+    {
+        return inertia('Payments/Create', [
+            'pageSettings' => fn() => [
+                'title' => 'Tambah Metode Pembayaran',
+                'subtitle' => 'Buat metode pembayaran baru disini. Klik simpan setelah selesai.',
+                'method' => 'POST',
+                'action' => route('payments.store'),
+            ],
+            'items' => fn() => [
+                ['label' => 'Cuan+', 'href' => route('dashboard')],
+                ['label' => 'Metode Pembayaran', 'href' => route('payments.index')],
+                ['label' => 'Tambah Metode Pembayaran'],
+            ],
+            'paymentTypes' => fn() => PaymentType::options(),
+        ]);
+    }
+
+
+    // method store
+    public function store(PaymentRequest $request): RedirectResponse
+    {
+        try {
+            Payment::create([
+                'user_id' => Auth::id(),
+                'name' => $request->name,
+                'type' => $request->type,
+                'account_number' => $request->account_number,
+                'account_owner' => $request->account_owner,
+            ]);
+
+            flashMessage(MessageType::CREATED->message('Metode Pembayaran'));
+            return to_route('payments.index');
+        } catch (Throwable $e) {
+            flashMessage(MessageType::ERROR->message(error: $e->getMessage()), 'error');
+            return to_route('payments.index');
+        }
     }
 }
---
+++ resources/js/Pages/Payments/Create.jsx
@@ -0,0 +1,139 @@
+import BreadcrumbHeader from '@/Components/BreadcrumbHeader';
+import HeaderTitle from '@/Components/HeaderTitle';
+import InputError from '@/Components/InputError';
+import { Button } from '@/Components/ui/button';
+import { Card, CardContent, CardHeader } from '@/Components/ui/card';
+import { Input } from '@/Components/ui/input';
+import { Label } from '@/Components/ui/label';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/Components/ui/select';
+import AppLayout from '@/Layouts/AppLayout';
+import { flashMessage } from '@/lib/utils';
+import { Link, useForm } from '@inertiajs/react';
+import { IconArrowBack, IconCheck, IconCreditCardPay, IconMoneybag } from '@tabler/icons-react';
+import { toast } from 'sonner';
+
+export default function Create(props) {
+    // destruct
+    const { data, setData, errors, post, processing, reset } = useForm({
+        name: '',
+        type: '',
+        account_number: '',
+        account_owner: '',
+        method: props.pageSettings.method,
+    });
+
+    const onHandleChange = (e) => setData(e.target.name, e.target.value);
+    const onHandleSubmit = (e) => {
+        e.preventDefault();
+        post(props.pageSettings.action, {
+            preserveScroll: true,
+            preserveState: true,
+            onSuccess: (success) => {
+                const flash = flashMessage(success);
+                if (flash) toast[flash.type](flash.message);
+            },
+        });
+    };
+
+    return (
+        <div className="flex w-full flex-col gap-y-6 pb-32">
+            <BreadcrumbHeader items={props.items} />
+
+            <Card>
+                <CardHeader>
+                    <div className="flex flex-col items-start justify-between gap-y-4 lg:flex-row lg:items-center">
+                        <HeaderTitle
+                            title={props.pageSettings.title}
+                            subtitle={props.pageSettings.subtitle}
+                            icon={IconCreditCardPay}
+                        />
+
+                        <Button variant="emerald" size="xl" asChild>
+                            <Link href={route('payments.index')}>
+                                <IconArrowBack size="4" />
+                                Kembali
+                            </Link>
+                        </Button>
+                    </div>
+                </CardHeader>
+                <CardContent>
+                    <form className="space-y-4" onSubmit={onHandleSubmit}>
+
+                        <div className="flex flex-col gap-2">
+                            <Label htmlFor="type">Tipe</Label>
+                            <Select value={data.type} onValueChange={(value) => setData('type', value)}>
+                                <SelectTrigger>
+                                    <SelectValue>
+                                        {props.paymentTypes.find((type) => type.value == data.type)
+                                            ?.label ?? "Pilih Tipe"}
+                                    </SelectValue>
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {props.paymentTypes.map((type, index) => (
+                                        <SelectItem key={index} value={type.value}>
+                                            {type.label}
+                                        </SelectItem>
+                                    ))}
+
+                                </SelectContent>
+                            </Select>
+                            {errors.type && <InputError message={errors.type} />}
+                        </div>
+
+                        <div className="flex flex-col gap-2">
+                            <Label htmlFor="name">Nama</Label>
+                            <Input
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder="Masukkan nama"
+                                value={data.name}
+                                onChange={onHandleChange}
+                            />
+                            {errors.name && <InputError message={errors.name} />}
+                        </div>
+
+                        <div className="flex flex-col gap-2">
+                            <Label htmlFor="account_number">Nomor Rekening</Label>
+                            <Input
+                                type="text"
+                                name="account_number"
+                                id="account_number"
+                                placeholder="Masukkan nomor rekening"
+                                value={data.account_number}
+                                onChange={onHandleChange}
+                            />
+                            {errors.account_number && <InputError message={errors.account_number} />}
+                        </div>
+
+                        <div className="flex flex-col gap-2">
+                            <Label htmlFor="account_owner">Nama Rekening</Label>
+                            <Input
+                                type="text"
+                                name="account_owner"
+                                id="account_owner"
+                                placeholder="Masukkan nama rekening"
+                                value={data.account_owner}
+                                onChange={onHandleChange}
+                            />
+                            {errors.account_onwer && <InputError message={errors.account_onwer} />}
+                        </div>
+
+
+                        <div className="mt-8 flex flex-col gap-2 lg:flex-row lg:justify-end">
+                            <Button type="button" variant="ghost" size="xl" onClick={() => reset()}>
+                                Reset
+                            </Button>
+                            <Button type="submit" variant="emerald" size="xl" disabled={processing}>
+                                <IconCheck />
+                                Submit
+                            </Button>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
+
+Create.layout = (page) => <AppLayout title={page.props.pageSettings.title} children={page} />;

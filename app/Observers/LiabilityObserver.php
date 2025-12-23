c33fa8347dcbf9f3b605e259e628d181f32eeb3f
raflimuhammadd <raflimuhh09@gmail.com>
2025-12-23 22:23:59 +0700

    Fix: calculation in LiabilityObserver during deletion

--- app/Observers/LiabilityObserver.php
+++ app/Observers/LiabilityObserver.php
@@ -9,13 +9,14 @@ class LiabilityObserver
     public function deleting(Liability $liability)
     {
         $netWorth = $liability->netWorth;
-        $totalLiability = $netWorth->liabilities()->sum('nominal');
-        
+        // calculate the total liability of the specific liability being deleted
+        $totalLiability = $liability->netWorthLiabilities->sum('nominal');
+
         $newNetWorth = $netWorth->current_net_worth + $totalLiability;
         $netWorth->update([
             'current_net_worth' => $newNetWorth,
             'amount_left' => $netWorth->goal - $newNetWorth,
-        ]); 
+        ]);
 
         $liability->netWorthLiabilities()->delete();
     }
